17ba4875424979d42aa5b885183178b7575c9ea7
at-yenho <hothiyen30@gmail.com>
2017-07-29 21:07:49 +0700

    init model 1

---
+++ app/Model/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Model
+{
+    // use Notifiable;
+
+    // /**
+    //  * The attributes that are mass assignable.
+    //  *
+    //  * @var array
+    //  */
+    // protected $fillable = [
+    //     'name', 'email', 'password',
+    // ];
+
+    // /**
+    //  * The attributes that should be hidden for arrays.
+    //  *
+    //  * @var array
+    //  */
+    // protected $hidden = [
+    //     'password', 'remember_token',
+    // ];
+}
