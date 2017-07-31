9ee31624177c6354fc53be79d02cfd0a4e3dac96
at-yenho <hothiyen30@gmail.com>
2017-07-31 11:52:21 +0700

    init model 2

--- app/Model/HotelService.php
+++ app/Model/HotelService.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class HotelService extends Model
 {
-    public $timestamps = false;
+	
 }
--- app/Model/User.php
+++ app/Model/User.php
@@ -8,23 +8,5 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Model
 {
-    // use Notifiable;
-
-    // /**
-    //  * The attributes that are mass assignable.
-    //  *
-    //  * @var array
-    //  */
-    // protected $fillable = [
-    //     'name', 'email', 'password',
-    // ];
-
-    // /**
-    //  * The attributes that should be hidden for arrays.
-    //  *
-    //  * @var array
-    //  */
-    // protected $hidden = [
-    //     'password', 'remember_token',
-    // ];
+    
 }
