2c809685750483bff73028bdee0ca759f8d9cd3d
at-annguyen <ngqanlc1995@gmail.com>
2017-08-02 17:30:29 +0700

    Update structe 3

--- resources/views/backend/layouts/main.blade.php
+++ resources/views/backend/layouts/main.blade.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | @yield('title') </title>
+
+  <title> Admin | @yield('title') </title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
@@ -32,12 +33,6 @@
   <!-- bootstrap wysihtml5 - text editor -->
   <link rel="stylesheet" href="{{ asset('bower_components/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
 
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
   <div class="wrapper">
@@ -60,11 +55,5 @@
   <!-- start js -->
   @include('backend.layouts.partials.js')
   <!-- end js -->
-<script >
-  $(function () {
-    $("#example1").DataTable();
-  })
-</script>
-
 </body>
 </html>
