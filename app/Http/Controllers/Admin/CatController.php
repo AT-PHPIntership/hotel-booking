22dcee972f68e58ff9b4897eb0621db83978df6e
at-annguyen <ngqanlc1995@gmail.com>
2017-08-04 13:50:14 +0700

    list categories

--- app/Http/Controllers/Admin/CatController.php
+++ app/Http/Controllers/Admin/CatController.php
@@ -15,73 +15,7 @@ class CatController extends Controller
      */
     public function index()
     {
-        $categories = Category::orderBy('id','DESC')->get();
-        return view('backend.categories.index',compact('categories'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $categories = Category::orderBy('id', 'DESC')->get();
+        return view('backend.categories.index', compact('categories'));
     }
 }
