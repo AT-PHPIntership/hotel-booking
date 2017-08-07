af8a5402d79f84b18f61030a21f7c6dcde46d9ab
at-annguyen <ngqanlc1995@gmail.com>
2017-08-07 14:15:58 +0700

    Delete Category

--- app/Http/Controllers/Admin/CatController.php
+++ app/Http/Controllers/Admin/CatController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Model\Category;
+use Session;
+use App\Model\News;
 
 class CatController extends Controller
 {
@@ -17,5 +19,24 @@ class CatController extends Controller
     {
         $categories = Category::orderBy('id', 'DESC')->get();
         return view('backend.categories.index', compact('categories'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id choose category delete id = $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if (Category::findOrFail($id)->delete()) {
+            if (News::with('category')->delete()) {
+                flash('Delete succeed')->success();
+            }
+        } else {
+            flash('Delete fail')->error();
+        }
+        return redirect()->route('category.index');
     }
 }
