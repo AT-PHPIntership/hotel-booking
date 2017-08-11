9aa0c6de0e015034b041207347a3ebcdbfe82f85
Nam <at-namnguyen>
2017-08-11 16:26:37 +0700

    create hotel

--- app/Http/Controllers/Admin/HotelController.php
+++ app/Http/Controllers/Admin/HotelController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Model\Hotel;
+use App\Model\Place;
+use App\Model\Service;
+use App\Http\Requests\Backend\HotelCreateRequest;
 
 class HotelController extends Controller
 {
@@ -26,5 +29,34 @@ class HotelController extends Controller
             ->paginate(Hotel::ROW_LIMIT);
 
         return view('backend.hotels.index', compact('hotels'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $places = Place::select('id', 'name')->get();
+        $services = Service::select('id', 'name')->get();
+
+        return view('backend.hotels.create', compact('places', 'services'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+         
+    // $name = $request->input('name');
+        dd($request->all());
+        //  if($request->ajax()){
+        //     print_r($request->all());
+        // }
+        // return route('hotel.index');
     }
 }
---
+++ resources/views/backend/hotels/create.blade.php
@@ -0,0 +1,100 @@
+@extends('backend.layouts.main')
+@section('title', __('Create Hotel'))
+@section('content')
+  <div class="content-wrapper">
+    <h1 class="title_page">{{__('Add hotel')}}</h1>
+    <section class="content">
+      <div class="row margin_center">
+        <div class="col-md-12">
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">{{__('Fill in form, please!')}}</h3>
+            </div>
+            <form role="form" method="POST" action="{{ route('hotel.store') }}" >
+              {{csrf_field()}}
+              <div class="box-body"> 
+                {{-- input hotel name --}}
+                <div class="form-group" {{ $errors->has('name') ? ' has-error' : '' }}>
+                  <input type="text" class="form-control" name="name" placeholder="{{ __('Hotel name') }}" value="{{ old('name') }}">
+                  @if($errors->first('name'))
+                    <span class="text-danger">{{ $errors->first('name') }}</span>
+                  @endif
+                </div>
+                {{-- input address --}}
+                <div class="form-group" {{ $errors->has('address') ? ' has-error' : '' }}>
+                  <input type="text" class="form-control" name="address" placeholder="{{ __('Address') }}" value="{{ old('address') }}">
+                  @if($errors->first('address'))
+                    <span class="text-danger">{{ $errors->first('address') }}</span>
+                  @endif
+                </div>
+                {{-- select place adn star --}}
+                <div class="form-inline">
+                  {{-- place select --}}
+                  <div class="form-group" {{ $errors->has('place') ? ' has-error' : '' }}>
+                    <select class="form-control" name="place">
+                      <option value="">Choose Place</option>
+                      @foreach($places as $place)
+                        <option value="{{ $place->id }}}">{{ $place->name }}</option>
+                      @endforeach
+                    </select>
+                    @if($errors->first('place'))
+                      <span class="text-danger">{{ $errors->first('place') }}</span>
+                    @endif
+                  </div>
+                   {{-- select star --}}
+                  <div class="form-group" {{ $errors->has('star') ? ' has-error' : '' }}>
+                    <select class="form-control" name="star">
+                      <option value="">Star</option>
+                      @for($i = 1; $i <= 5; $i++ )
+                        <option value="{{ $i }}">{{ $i }}</option>
+                      @endfor
+                    </select>
+                    @if($errors->first('star'))
+                      <span class="text-danger">{{$errors->first('star')}}</span>
+                    @endif
+                  </div>
+                </div>
+                {{-- introduce --}}
+                <label></label>
+                <div class="form-group" {{ $errors->has('introduce') ? ' has-error' : '' }}>
+                  <textarea class="form-control" name="introduce" placeholder="{{ __('Introduction about hotel') }}" value="{{ old('introduce') }}"></textarea>
+                  @if($errors->first('introduce'))
+                    <span class="text-danger">{{$errors->first('introduce')}}</span>
+                  @endif
+                </div>
+                {{-- Services --}}
+                <div class="form-group">
+                <p><b>{{ __('Choose Services') }}</b></p>
+                  @foreach($services as $service)
+                    <div class="checkbox-inline">
+                      <label><input type="checkbox" onclick="" value="{{ $service->id.'-'.$service->name }}">{{ $service->name }}</label>
+                    </div>
+                  @endforeach
+                </div>
+                {{-- choose --}}
+                <div class="form-group">
+                <p><b>{{ __('Choosed:') }}</b></p>
+                  <input type="text" readonly="readonly" id="services" value="" name="services" class="form-control">
+                </div>
+                {{-- upload image --}}
+                <div class="form-group">
+                  <input type="file" class="form-control" name='imgs[]' id="img-upload" multiple placeholder="{{ __('Images') }}" value="{{ old('img') }}">
+                   <div class="preview-img">
+                   </div>
+                </div>
+              </div>
+              <div class="box-footer btn-add-news">
+                <button type="reset" class="btn btn-primary">
+                  {{__('Reset')}}
+                </button>
+                <button type="submit" class="btn btn-primary" id="js-bt-submit">
+                  {{__('Submit')}}
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+@endsection
