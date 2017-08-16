63103429ef4329ed1501ce729af3fd01e22db80e
Duoc <satthukt1232@gmail.com>
2017-08-16 12:39:56 +0700

    Trait Search

--- app/Libraries/Traits/SearchTrait.php
+++ app/Libraries/Traits/SearchTrait.php
@@ -6,45 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 trait SearchTrait {
 
-    /**
-     * @param \Illuminate\Database\Eloquent\Builder|static $query
-     * @param string $keyword
-     * @param boolean $matchAllFields
-     */
-    public static function search($query, $keyword, $matchAllFields = false)
-    {
-        return static::where(function ($query) use ($keyword, $matchAllFields) {
-            foreach (static::getSearchFields() as $field) {
-                if ($matchAllFields) {
-                    $query->where($field, 'LIKE', "%$keyword%");
-                // } else if($matchAllFields) {
-                //     $query->orWhere($field, 'LIKE', "%$keyword%");
-                // } else {
-                //     $query->orWhere($field, 'LIKE', "%$keyword%");
-                }
-            }
-        });
-    }
-
-    /**
-     * Get all searchable fields
+      /**
+     * Scope a query to only include popular users.
      *
-     * @return array
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
      */
-    public static function getSearchFields()
+    public function scopeSearch($query)
     {
-        $model = new static;
-        $fields = $model->search;
-        if (empty($fields)) {
-            $fields = Schema::getColumnListing($model->getTable());
-            $others[] = $model->primaryKey;
-            $others[] = $model->getUpdatedAtColumn() ?: 'created_at';
-            $others[] = $model->getCreatedAtColumn() ?: 'updated_at';
-            $others[] = method_exists($model, 'getDeletedAtColumn')
-                ? $model->getDeletedAtColumn()
-                : 'deleted_at';
-            $fields = array_diff($fields, $model->getHidden(), $others);
+        $keyword = request('search');
+        foreach ($this->searchField as $value) {
+            $query->orWhere($value, "LIKE", "%$keyword%");
         }
-        return $fields;
-    }
+    }  
 }
