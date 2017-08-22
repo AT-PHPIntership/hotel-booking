777c78dd7bad9deb5963eb642c50c25a06c1b2fb
Nam <at-namnguyen>
2017-08-22 10:14:29 +0700

    Test delete comment

---
+++ tests/Browser/Pages/Backend/RatingComments/AdminDeleteCommentTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Tests\Browser\Pages\Backend\RatingComments;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Faker\Factory as Faker;
+use App\Model\Hotel;
+use App\Model\User;
+use App\Model\RatingComment;
+use App\Model\Place;
+
+class AdminDeleteCommentTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A Dusk test link & content.
+     *
+     * @return void
+     */
+    public function testListRatingComment()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/admin')
+                    ->clickLink('Comment and Rating')
+                    ->assertSee('List comment & rating')
+                    ->assertPathIs('/admin/comment');
+        });
+    }
+
+    /**
+     * Test delete comment success
+     *
+     * @return void
+     */
+    public function testDeleteSuccess() 
+    {
+        $this->makeData(5);
+        $this->browse(function (Browser $browser) {
+            $page = $browser->visit('/admin/comment');
+            $elements = $page->elements('#list-table tbody tr');
+            $this->assertCount(5, $elements);
+            $page->press('#list-table tbody tr:nth-child(4) td:nth-child(8) button')
+            ->waitForText("Confirm deletion!")
+            ->press('Delete')
+            ->assertSee("Deletion successful");
+            $this->assertSoftDeleted('rating_comments', ['id' => '2']);
+            $elements = $page->elements('#list-table tbody tr');    
+            $this->assertCount(4, $elements);    
+        });
+    } 
+
+    /**
+     * Test not found when delete
+     *
+     * @return void
+     */
+    public function testNotFound()
+    {   
+        $this->makeData(5);
+        $this->browse(function (Browser $browser) {
+            $page = $browser->visit('/admin/comment');
+            $rating_comments = RatingComment::find(2);
+            $rating_comments->delete();
+            $page->press('#list-table tbody tr:nth-child(4) td:nth-child(8) button')
+                ->waitForText("Confirm deletion!")
+                ->press('Delete')
+                ->assertSee("404 - Page Not found");
+        });  
+    }
+
+    /**
+     * Make data for test.
+     *
+     * @return void
+     */
+    public function makeData($row)
+    {   
+        factory(Place::class, 10)->create();
+
+        $placeIds = Place::all('id')->pluck('id')->toArray();
+        $faker = Faker::create();
+        for ($i = 0; $i < 10; $i++) {
+            factory(Hotel::class, 1)->create([
+                'place_id' => $faker->randomElement($placeIds)
+            ]);
+        }
+
+        factory(User::class, 10)->create();
+        $hotelIds = Hotel::all('id')->pluck('id')->toArray();
+        $userIds = User::all('id')->pluck('id')->toArray();
+        $faker = Faker::create();
+        for ($i = 0; $i < $row; $i++) {
+            factory(RatingComment::class, 1)->create([
+                'hotel_id' => $faker->randomElement($hotelIds),
+                'user_id' => $faker->randomElement($userIds)
+            ]);
+        }
+    }
+}
