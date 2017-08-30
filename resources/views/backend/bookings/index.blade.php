e19145cfa9d23c36479061a694ecfd0494c34e7d
Nhat Trung <trantrungqn91@gmail.com>
2017-08-31 05:21:45 +0700

    Update template booking room

--- resources/views/backend/bookings/index.blade.php
+++ resources/views/backend/bookings/index.blade.php
@@ -8,7 +8,7 @@
       </h1>
       <ol class="breadcrumb">
         <li>
-          <a href="{{ route('admin.index') }}"><i class="fa fa-dashboard"></i>{{__('Home')}}</a>
+          <a href="#"><i class="fa fa-dashboard"></i>{{__('Home')}}</a>
         </li>
         <li class="active">{{__('Booking Rooms')}}</li>
       </ol>
@@ -30,7 +30,7 @@
             </div>
             <div class="box-body">
               @include('flash::message')
-              @include('backend.layouts.partials.modal')
+              {{-- @include('backend.layouts.partials.modal') --}}
               <table class="table table-bordered table-responsive table-striped" id="table-contain">
                 <thead>
                 <tr>
@@ -67,18 +67,22 @@
                     <td>{{$reservation->checkout_date}}</td>
                     <td>{{$reservation->status_label}}</td>
                     <td align="center">
-                      <a href="{{ route('reservation.show', $reservation->id) }}" data-original-title="Detail" data-toggle="tooltip" class="btn fa fa-search-plus pull-left news-btn">
-                      </a>
-                      <a href="{{ route('reservation.edit', $reservation->id) }}" class="btn fa fa-pencil-square-o news-btn center-block" data-original-title="Edit" data-toggle="tooltip" >
-                      </a>
-                      <form action="{{ route('reservation.destroy', $reservation->id) }}" method="POST" class="inline">
-                        {{csrf_field()}}
-                        {{method_field('DELETE')}}
-                        <button type="submit" class="news-btn fa fa-trash-o btn-delete-item pull-right"
-                         data-original-title="Delete" data-toggle="tooltip"  data-title="{{ __('Confirm deletion!') }}"
-                            data-confirm="{{ __('Are you sure you want to delete?') }}">
-                        </button>
-                      </form>
+                      <div class="text-center">
+                        <a href="{{ route('reservation.show', $reservation->id) }}" data-original-title="Detail" data-toggle="tooltip" class="btn fa fa-search-plus pull-left news-btn btn-custom-option">
+                        </a>
+                        @if($reservation->status_label != __('Canceled'))
+                          <a href="" class="btn fa fa-pencil-square-o news-btn center-block btn-custom-option" data-original-title="Edit" data-toggle="tooltip" >
+                          </a>
+                        @endif
+                        <form action="" method="POST" class="inline">
+                          {{csrf_field()}}
+                          {{method_field('DELETE')}}
+                          <button type="submit" class=" btn-custom-option news-btn fa fa-trash-o btn-delete-item pull-right"  
+                           data-original-title="Delete" data-toggle="tooltip"  data-title="{{ __('Confirm deletion!') }}"
+                              data-confirm="{{ __('Are you sure you want to delete?') }}">
+                          </button>
+                        </form>
+                      </div>
                     </td>
                   </tr>
                   @endforeach
