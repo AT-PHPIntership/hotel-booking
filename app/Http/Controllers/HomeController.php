0d757ddbf958a518dc973f22b864639d7b41a54f
Nam <at-namnguyen>
2017-09-07 09:21:19 +0700

    user homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\Hotel;
+use App\Model\Room;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,38 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('frontend.home.index');
+        $columns = [
+            'id',
+            'name',
+            'address',
+            'star',
+            'introduce',
+            'place_id',
+        ];
+
+        $with['place'] = function ($query) {
+            $query->select('id', 'name');
+        };
+        $with['rooms'] = function ($query) {
+            $query->select('hotel_id', 'id', 'name')->with('reservations');
+        };
+        $with['images'] = function ($query) {
+            $query->select();
+        };
+        $with['hotelServices'] = function ($query) {
+            $query->select('id', 'hotel_id', 'service_id');
+        };
+        $with['hotelServices.service'] = function ($query) {
+            $query->select('id', 'name');
+        };
+
+        $hotels = Hotel::select($columns)->with($with)->limit(4)->orderby('id', 'DESC')->get();
+        // dd($hotels);
+        // $countRoom = Room::withCount('reservations')->get();
+        foreach ($hotels as $key) {
+            echo($key->rooms->count('reservations'));
+        }
+        
+        // return view('frontend.home.index');
     }
 }
