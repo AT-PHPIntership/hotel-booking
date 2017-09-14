ce9f57e5046d1e887ab0c525fd02d6c19c6cadde
Duoc <satthukt1232@gmail.com>
2017-09-14 13:41:17 +0700

    Feedback

---
+++ app/Http/Controllers/Frontend/FeedBackController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Frontend\FeedbackRequest;
+use App\Model\Feedback;
+
+class FeedBackController extends Controller
+{
+    /**
+     * Create feedback.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('frontend.feedbacks.index');
+    }
+
+    /**
+     * Store a newly feedback in storage.
+     *
+     * @param \Illuminate\Http\CreateNewsRequest $request of form creat News
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(FeedbackRequest $request)
+    {
+        $feedback = new Feedback($request->all());
+        $result = $feedback->save();
+        if ($result) {
+            flash(__('Sent Feedback!'))->success();
+            return redirect()->route('feedback');
+        } else {
+            flash(__('Error when send feedback!'))->error();
+            return redirect()->route('feedback');
+        }
+    }
+}
