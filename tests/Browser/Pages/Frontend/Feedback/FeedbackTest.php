94f77d91c8702ca046d2d754f02941a8c282a733
Duoc <satthukt1232@gmail.com>
2017-09-16 16:27:21 +0700

    Fix conflict

--- tests/Browser/Pages/Frontend/Feedback/FeedbackTest.php
+++ tests/Browser/Pages/Frontend/Feedback/FeedbackTest.php
@@ -7,6 +7,7 @@ use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Support\Facades\Auth;
 use App\Model\Feedback;
+use App\Model\User;
 
 class FeedbackTest extends DuskTestCase
 {   
@@ -24,94 +25,157 @@ class FeedbackTest extends DuskTestCase
                     ->visit('/')
                     ->clickLink('Feedback')
                     ->assertSee('Feedback')
-                    ->assertPathIs('/feedback');
+                    ->assertPathIs('/sendfeedback/create');
         });
     }
 
     /**
-     * Test value of page feedback.
+     * Test value of page feedback if guest visit page.
      *
      * @return void
      */
-    public function testValuePageFeedBack()
+    public function testValuePageFeedBackIfNotLogin()
     {
         $this->browse(function (Browser $browser) {
             $browser->logout()
-                    ->visit('/feedback')
-                    ->assertSee('Feedback');
-            if (Auth::user()) {
-                $browser->assertInputValue('full_name', Auth::user()->full_name)
-                        ->assertInputValue('email', Auth::user()->email)
-                        ->assertInputValue('content', '')
-                        ->assertPathIs('/feedback');
-            } else {
-                $browser->assertInputValue('full_name', '')
-                        ->assertInputValue('email', '')
-                        ->assertInputValue('content', '')
-                        ->assertPathIs('/feedback');
-            }
+                    ->visit('/sendfeedback/create')
+                    ->assertSee('Feedback')
+                    ->assertInputValue('full_name', '')
+                    ->assertInputValue('email', '')
+                    ->assertInputValue('content', '')
+                    ->assertPathIs('/sendfeedback/create');
         });
     }
 
     /**
-     * Test send feedback Success.
+     * Test value of page feedback if user visit page.
      *
      * @return void
      */
-    public function testFeedBackSuccess()
+    public function testValuePageFeedBackIfLogin()
+    {   
+        $this->makeData();
+        $this->browse(function (Browser $browser) {
+            $user = User::find(2);
+            $browser->loginAs($user)
+                    ->visit('/sendfeedback/create')
+                    ->assertSee('Feedback');
+            $browser->assertInputValue('full_name', $user->full_name)
+                    ->assertInputValue('email', $user->email)
+                    ->assertInputValue('content', '')
+                    ->assertPathIs('/sendfeedback/create');              
+        });
+    }
+
+    /**
+     * Test send feedback success of guest.
+     *
+     * @return void
+     */
+    public function testFeedBackSuccessIfNotLogin()
     {
         $this->browse(function (Browser $browser) {
             $browser->logout()
-                    ->visit('/feedback')
+                    ->visit('/sendfeedback/create')
+                    ->assertSee('Feedback')
+                    ->type('full_name', 'Duoc Nguyen')
+                    ->type('email', 'duocnguyen@example.com')
+                    ->type('content', 'Very good. I like your website')
+                    ->press('SUBMIT')
+                    ->assertSee('Sent Feedback!')
+                    ->assertPathIs('/sendfeedback/create');
+            $this->assertDatabaseHas('feedbacks', [
+                'full_name' => 'Duoc Nguyen',
+                'email' => 'duocnguyen@example.com',
+                'content' => 'Very good. I like your website'
+            ]);
+        });
+    }
+
+    /**
+     * Test send feedback success if user login and don't change value of filed name, email.
+     *
+     * @return void
+     */
+    public function testFeedBackSuccessIfLoginAndNotChangeField()
+    {
+        $this->makeData();
+        $this->browse(function (Browser $browser) {
+            $user = User::find(2);
+            $browser->loginAs($user)
+                    ->visit('/sendfeedback/create')
                     ->assertSee('Feedback');
-            if (Auth::user()) {
-                $browser->type('content', 'Very good')
+                $browser->type('content', 'Very good. I like your website')
                         ->press('SUBMIT')
                         ->assertSee('Sent Feedback!')
-                        ->assertPathIs('/feedback');
+                        ->assertPathIs('/sendfeedback/create');   
                 $this->assertDatabaseHas('feedbacks', [
-                    'full_name' => Auth::user()->full_name,
-                    'email' => Auth::user()->email,
-                    'content' => 'Very good'
-                ]); 
-            } else {
-                $browser->type('full_name', 'Duoc Nguyen')
-                        ->type('email', 'duocnguyen@example.com')
-                        ->type('content', 'Very good. I like your website')
-                        ->press('SUBMIT')
-                        ->assertSee('Sent Feedback!')
-                        ->assertPathIs('/feedback');
-                $this->assertDatabaseHas('feedbacks', [
-                    'full_name' => 'Duoc Nguyen',
-                    'email' => 'duocnguyen@example.com',
+                    'full_name' => $user->full_name,
+                    'email' => $user->email,
                     'content' => 'Very good. I like your website'
                 ]);
-            }
         });
     }
 
     /**
-     * Test validation page feedback.
+     * Test send feedback success if user login and change value of filed name, email.
      *
      * @return void
      */
-    public function testValidationFeedBack()
+    public function testFeedBackSuccessIfLoginAndChangeField()
+    {   
+        $this->makeData();
+        $this->browse(function (Browser $browser) {
+            $user = User::find(2);
+            $browser->loginAs($user)
+                    ->visit('/sendfeedback/create')
+                    ->assertSee('Feedback');
+                $browser->type('full_name', 'Duoc Nguyen C.')
+                        ->type('email', 'duocduoc@gmail.com')
+                        ->type('content', 'I am Duoc. Your website is very good. I like your website.')
+                        ->press('SUBMIT')
+                        ->assertSee('Sent Feedback!')
+                        ->assertPathIs('/sendfeedback/create');   
+                $this->assertDatabaseHas('feedbacks', [
+                    'full_name' => 'Duoc Nguyen C.',
+                    'email' => 'duocduoc@gmail.com',
+                    'content' => 'I am Duoc. Your website is very good. I like your website.'
+                ]);
+        });
+    }
+
+    /**
+     * Test validation page feedback of guest.
+     *
+     * @return void
+     */
+    public function testValidationFeedBackIfNotLogin()
     {
         $this->browse(function (Browser $browser) {
             $browser->logout()
-                    ->visit('/feedback')
-                    ->assertSee('Feedback');
-            if (Auth::user()) {
-                $browser->press('SUBMIT')
-                        ->assertSee('The content field is required.')
-                        ->assertPathIs('/feedback');
-            } else {
-                $browser->press('SUBMIT')
-                        ->assertSee('The full name field is required.')
-                        ->assertSee('The email field is required.')
-                        ->assertSee('The content field is required.')
-                        ->assertPathIs('/feedback');
-            }
+                    ->visit('/sendfeedback/create')
+                    ->assertSee('Feedback')
+                    ->press('SUBMIT')
+                    ->assertSee('The full name field is required.')
+                    ->assertSee('The email field is required.')
+                    ->assertSee('The content field is required.')
+                    ->assertPathIs('/sendfeedback/create');
+        });
+    }
+
+    /**
+     * Test validation page feedback of user.
+     *
+     * @return void
+     */
+    public function testValidationFeedBackIfLogin()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/sendfeedback/create')
+                    ->assertSee('Feedback')
+                    ->press('SUBMIT')
+                    ->assertSee('The content field is required.')
+                    ->assertPathIs('/sendfeedback/create');
         });
     }
 
@@ -137,44 +201,63 @@ class FeedbackTest extends DuskTestCase
     public function testValidation($name, $email, $content, $expected)
     {
         $this->browse(function (Browser $browser) use ($name, $email, $content, $expected){
-            $browser->visit('/feedback')
+            $browser->visit('/sendfeedback/create')
                     ->assertSee('Feedback')
                     ->type('full_name', $name)
                     ->type('email', $email)
                     ->type('content', $content)
                     ->press('SUBMIT')
                     ->assertSee($expected)
-                    ->assertPathIs('/feedback');
+                    ->assertPathIs('/sendfeedback/create');
         });
     }
 
     /**
-     * Test button Reset.
+     * Test button Reset of Guest.
      *
      * @return void
      */
-    public function testButtonReset()
+    public function testButtonResetIfNotLogin()
     {
         $this->browse(function (Browser $browser) {
             $browser->logout()
-                    ->visit('/feedback')
-                    ->assertSee('Feedback');
-            if (Auth::user()) {
-                $browser->type('content', 'Very good. I like it')
-                        ->press('RESET')
-                        ->assertSee('The content field is required.')
-                        ->assertInputValue('content', '')
-                        ->assertPathIs('/feedback');
-            } else {
-                $browser->type('full_name', 'Duoc Nguyen')
-                        ->type('email', 'duocnguyen@example.com')
-                        ->type('content', 'Very good. I like your website')
-                        ->press('RESET')
-                        ->assertInputValue('full_name', '')
-                        ->assertInputValue('email', '')
-                        ->assertInputValue('content', '')
-                        ->assertPathIs('/feedback');
-            }
+                    ->visit('/sendfeedback/create')
+                    ->assertSee('Feedback')
+                    ->type('full_name', 'Duoc Nguyen')
+                    ->type('email', 'duocnguyen@example.com')
+                    ->type('content', 'Very good. I like your website')
+                    ->press('RESET')
+                    ->assertInputValue('full_name', '')
+                    ->assertInputValue('email', '')
+                    ->assertInputValue('content', '')
+                    ->assertPathIs('/sendfeedback/create');
+        });
+    }
+
+      /**
+     * Test button Reset of user.
+     *
+     * @return void
+     */
+    public function testButtonResetIfLogin()
+    {
+        $this->makeData();
+        $this->browse(function (Browser $browser) {
+            $user = User::find(2);
+            $browser->loginAs($user)
+                    ->visit('/sendfeedback/create')
+                    ->assertSee('Feedback')
+                    ->assertInputValue('full_name', $user->full_name)
+                    ->assertInputValue('email', $user->email)
+                    ->assertInputValue('content', '')
+                    ->type('full_name', 'Nguyen Cong Duoc')
+                    ->type('email', 'duocduoc@example.com')
+                    ->type('content', 'Very good. I like it')
+                    ->press('RESET')
+                    ->assertInputValue('full_name', $user->full_name)
+                    ->assertInputValue('email', $user->email)
+                    ->assertInputValue('content', '')
+                    ->assertPathIs('/sendfeedback/create');
         });
     }
 
@@ -186,11 +269,27 @@ class FeedbackTest extends DuskTestCase
     public function testButtonBack()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/feedback')
+            $browser->visit('/sendfeedback/create')
                     ->assertSee('Feedback')
                     ->clickLink('Back')
                     ->assertSee('Outstanding Places')
                     ->assertPathIs('/');
         });
+    }
+
+    /**
+     * Make data for test
+     *
+     */
+    public function makeData()
+    {
+        User::create([
+            'username' => 'duocduoc',
+            'password' => bcrypt('duocduoc'),
+            'full_name' => 'Duoc Nguyen',
+            'is_active' => 1,
+            'email' => 'duocnguyen@gmail.com',
+            'phone' => '01206223029'
+        ]);
     }
 }
